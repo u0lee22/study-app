031f044076f541b858fee4a7af4a70512dfc5521
LeeWooYoung <u0lee22@gmail.com>
2017-09-19 10:41:06 +0900

    0919_

--- angular-study-app/src/app/route/route.config.js
+++ angular-study-app/src/app/route/route.config.js
@@ -28,6 +28,21 @@
                 templateUrl: 'app/home/test.html',
                 controller: 'homeController'
             })
+            .state('app.user', {
+                url: '/user',
+                abstract: true,
+                template: '<div ui-view=""></div>'
+            })
+            .state('app.user.create', {
+                url: '/create',
+                templateUrl: 'app/user/create.html',
+                controller: 'userListCtrl'
+            })
+            .state('app.user.list', {
+                url: '/list',
+                templateUrl: 'app/user/list.html',
+                controller: 'userListCtrl'
+            })
         ;
     }
 })();
