a69b657f02a59cf269aa7e9b9f9de403e091ee39
LeeWooYoung <u0lee22@gmail.com>
2017-09-26 15:18:50 +0900

    0921 filter 추가

---
+++ angular-study-app/src/app/app.config.js
@@ -0,0 +1,42 @@
+app.provider('validation', function () {
+    var objValidation;
+    var arrValidation;
+
+    return {
+        setArrValidation: function (arr) {
+            arr.forEach(function (item) {
+                arrValidation.push(this.setObjValidation(item.key, item.inValid, item.pattern, item.compareTo, item.valid));
+            });
+        },
+
+        setObjValidation: function (key, inValid, pattern, compareTo, valid) {
+            var patterns = {NOT_EMPTY: 'notEmpty'};
+
+            return objValidation = {
+                key: key,
+                inValid: inValid,
+                pattern: pattern == null || angular.isUndefined(pattern) ? patterns.NOT_EMPTY : pattern,
+                compareTo: compareTo,
+                valid: valid
+            };
+        },
+
+        $get: function () {
+            function getArrValidation() {
+                return arrValidation;
+            }
+
+            return {variable: getArrValidation};
+        }
+    };
+});
+
+
+angular.module('study.app')
+    .config(appConfig);
+
+appConfig.$inject = ['validationProvider'];
+
+function appConfig(validationProvider) {
+    validationProvider.setArrValidation();
+}
